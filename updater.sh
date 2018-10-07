41629fa53c2e6ea051a538d03339289bd709528a
dark-shade <sr282828@gmail.com>
2018-10-06 22:36:44 -0500

    upgrader utility complete

--- updater.sh
+++ updater.sh
@@ -1,4 +1,70 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get upgrade -yy
+# Upgrader -- Debian/Ubuntu Update Tool (Version 1.0)
+
+# Exit on error
+set -e
+
+# Functions
+
+update() {
+	echo "Starting full system update..."
+	sudo apt-get update
+	sudo apt-get full-upgrade -yy
+}
+
+clean() {
+	echo "Cleaning up..."
+	sudo apt-get autoremove -yy
+	sudo apt-get autoclean
+}
+
+leave()
+{
+	echo "----------------------"
+	echo "-  Update Complete!  -"
+	echo "----------------------"
+	exit
+}
+
+upgrader-help() {
+
+cat << _EOF_
+
+Usage: upgrader [Options]
+
+Upgrader is a tool that automates the update procedure for debian based systems.
+
+Options:
+	<No option>	Running with no options does full system update.
+	--clean		Full system update with cleanup.
+	--help		Shows this help page.
+
+_EOF_
+
+# Execution
+echo "Upgrader -- Debian/Ubuntu Update Tool (Version 1.0)"
+
+# Update and Clean
+if [ "$1" == "--clean" ]; then
+	update
+	clean
+	leave
+fi
+
+# Help
+if [ "$1" == "--help" ]; then
+	upgrader-help
+	exit
+fi
+
+# Check for invalid arguments
+if [ -n "$1" ]; then
+	echo "Upgrader Error: Invalid argument. Try 'upgrader --help' for more info." >&2
+	exit 1
+fi
+
+# Without arguments
+update
+leave
+	
