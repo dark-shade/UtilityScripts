c6862a992eaeebfb6998ea5596c97a3f911bb8c2
dark-shade <sr282828@gmail.com>
2018-10-21 11:36:44 -0500

    cloner completed

--- cloner.sh
+++ cloner.sh
@@ -21,27 +21,7 @@ create_deb_package(){
 	mkdir -p "$HOME/webupdpackage/debian"
 	equivs-control "$HOME/webupdpackage/debian/control"
 	pkgs=$(aptitude search -F %p ~i --disable-columns libedataserver | sed 's/$/,/' | tr '\n\r' ' ' | sed 's/, $//')
-	echo '### Commented entries have reasonable defaults.
-	### Uncomment to edit them.
-	Section: misc
-	Priority: optional
-	Standards-Version: 3.6.2
-
-	Package: webupd8package
-	Version: 1.0
-	Maintainer: Andrew < andrew@webupd8.org >
-	# Pre-Depends: <comma-separated list of packages>
-	Depends: '$pkgs'
-	# Recommends: <comma-separated list of packages>
-	# Suggests: <comma-separated list of packages>
-	# Provides: <comma-separated list of packages>
-	# Replaces: <comma-separated list of packages>
-	Architecture: all
-	# Copyright: <copyright file; defaults to GPL2>
-	# Changelog: <changelog file; defaults to a generic changelog>
-	# Readme: <README.Debian file; defaults to a generic one>
-	# Extra-Files: <comma-separated list of additional files for the doc directory>
-	Description: All my applications packed into a deb file' > "$HOME/webupdpackage/debian/control"
+	printf "### Commented entries have reasonable defaults.\n### Uncomment to edit them.\nSection: misc\nPriority: optional\nStandards-Version: 3.6.2\n\nPackage: webupd8package\nVersion: 1.0\nMaintainer: Andrew < andrew@webupd8.org >\n# Pre-Depends: <comma-separated list of packages>\nDepends: %s\n# Recommends: <comma-separated list of packages>\n# Suggests: <comma-separated list of packages>\n# Provides: <comma-separated list of packages>\n# Replaces: <comma-separated list of packages>\nArchitecture: all\n# Copyright: <copyright file; defaults to GPL2>\n# Changelog: <changelog file; defaults to a generic changelog>\n# Readme: <README.Debian file; defaults to a generic one>\n# Extra-Files: <comma-separated list of additional files for the doc directory>\nDescription: All my applications packed into a deb file" "$pkgs" > "$HOME/webupdpackage/debian/control"
 	equivs-build "$HOME/webupdpackage/debian/control"
 	cp *.deb .
 	rm -rf "$HOME/webupdpackage"
